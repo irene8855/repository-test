9b38a24c543b04dd6d8c2596df75d9cda55363ed
irene8855 <irentu8@gmail.com>
2025-07-25 23:38:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -294,6 +294,4 @@ def main_loop():
 def start_background_loop():
     threading.Thread(target=main_loop, daemon=True).start()
 
-@app.before_first_request
-def before_first_request():
-    start_background_loop()   
+  
