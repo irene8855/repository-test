deaf6943f9e07015b3c707b752f8191b081e902e
irene8855 <irentu8@gmail.com>
2025-08-27 18:34:58 +0100

    Create pipeline_web3.py

---
+++ pipeline_web3.py
@@ -0,0 +1,63 @@
+# pipeline_web3.py
+import os
+from web3 import Web3
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# RPC через Alchemy (Polygon)
+ALCHEMY_RPC = os.getenv("ALCHEMY_POLYGON_RPC")
+w3 = Web3(Web3.HTTPProvider(ALCHEMY_RPC))
+
+# Пример: пул USDT–MATIC на QuickSwap (Polygon)
+PAIR_ADDRESS = Web3.to_checksum_address("0x4d6b2b90fdb1c68b9b37c58d93d6309d63f03bc0")
+
+PAIR_ABI = [
+    {
+        "inputs": [],
+        "name": "getReserves",
+        "outputs": [
+            {"internalType": "uint112", "name": "_reserve0", "type": "uint112"},
+            {"internalType": "uint112", "name": "_reserve1", "type": "uint112"},
+            {"internalType": "uint32", "name": "_blockTimestampLast", "type": "uint32"},
+        ],
+        "stateMutability": "view",
+        "type": "function",
+    },
+]
+
+pair = w3.eth.contract(address=PAIR_ADDRESS, abi=PAIR_ABI)
+
+def get_quote_web3(src_symbol, dst_symbol, amount_units):
+    """
+    Возвращает данные в формате, как get_quote_ds / get_quote_1inch
+    Пока только для пула USDT–MATIC (пример).
+    """
+    try:
+        if not w3.is_connected():
+            return None
+
+        reserves = pair.functions.getReserves().call()
+        reserve0, reserve1 = reserves[0], reserves[1]
+
+        # Упрощение: считаем, что token0 = USDT (6 знаков), token1 = MATIC (18 знаков)
+        # В реальном коде нужно будет проверять порядок!
+        amount_in = amount_units * 10**6  # USDT -> wei
+        amount_out = (amount_in * reserve1) // (reserve0 + amount_in)
+
+        price = amount_out / 1e18 / (amount_units)  # цена MATIC в USDT
+        liquidity_usd = reserve0 / 1e6 + (reserve1 / 1e18 * price)
+
+        return {
+            "src_symbol": src_symbol,
+            "dst_symbol": dst_symbol,
+            "amount_in": amount_units,
+            "amount_out": amount_out / 1e18,
+            "price": price,
+            "liquidity_usd": liquidity_usd,
+            "source": "Web3",
+        }
+
+    except Exception as e:
+        print("Web3 error:", e)
+        return None
